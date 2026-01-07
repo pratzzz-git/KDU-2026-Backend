e3e5335283ac511f84c4550ffa446786cf5990a2
pratzzz2432 <pratzzz2432@gmail.com>
2026-01-07 14:29:24 +0530

    3-A question of hands on done

---
+++ src/main/java/org/example/asyncdemo/CallableFutureDemo.java
@@ -0,0 +1,25 @@
+package asyncdemo;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class CallableFutureDemo {
+
+    public static void main(String[] args) throws Exception {
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        CallableSumTask task = new CallableSumTask(10);
+
+        Future<Integer> future = executor.submit(task);
+
+        System.out.println("Task submitted, doing something else...");
+
+        Integer result = future.get();   // BLOCKING
+
+        System.out.println("Sum result: " + result);
+
+        executor.shutdown();
+    }
+}
